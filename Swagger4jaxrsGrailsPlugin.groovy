f7f1bcc176b95a082f4fd2dd37310226e74d96d7
Angel Ruiz <aruizca@gmail.com>
2013-10-04 18:25:30 +1000

    Now the version of Grails supported is 2.0 > * instead of 2.2 > *

--- Swagger4jaxrsGrailsPlugin.groovy
+++ Swagger4jaxrsGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.codehaus.groovy.grails.commons.GrailsApplication
 class Swagger4jaxrsGrailsPlugin {
     def groupId = 'com.nerderg.grails.plugins'
     def version = "0.1-SNAPSHOT"
-    def grailsVersion = "2.2 > *"
+    def grailsVersion = "2.0 > *"
     def pluginExcludes = [
         "web-app/WEB-INF/**"
     ]
