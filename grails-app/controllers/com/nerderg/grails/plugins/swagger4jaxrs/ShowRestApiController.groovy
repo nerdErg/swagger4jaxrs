f3d865d44c980658818493b8540405e00b20bd06
Aaron Brown <brown.aaron.lloyd@gmail.com>
2014-05-26 20:15:16 -0500

    Using Context Path.
    This is more appropriate for what needs to be done. The
    GrailsLinkGenerator created URLs based on hostname of server
    (obviously), but this means that the Try-It-Out function had odd
    behavior and did not use the DNS name.
    
    The Context Path solves that issue.

--- grails-app/controllers/com/nerderg/grails/plugins/swagger4jaxrs/ShowRestApiController.groovy
+++ grails-app/controllers/com/nerderg/grails/plugins/swagger4jaxrs/ShowRestApiController.groovy
@@ -2,18 +2,12 @@ package com.nerderg.grails.plugins.swagger4jaxrs
 
 import com.wordnik.swagger.jaxrs.config.BeanConfig
 
-import org.codehaus.groovy.grails.web.mapping.LinkGenerator
-
 class ShowRestApiController {
-
     BeanConfig swaggerConfig
-    LinkGenerator grailsLinkGenerator
 
     def index() {
-        String basePath = grailsLinkGenerator.serverBaseURL
+        swaggerConfig.basePath = request.contextPath
 
-        swaggerConfig.basePath = basePath
-
-        render(view: 'index', model: [apiDocsPath: "${basePath}/api-docs"])
+        render(view: 'index', model: [apiDocsPath: "${request.contextPath}/api-docs"])
     }
 }
